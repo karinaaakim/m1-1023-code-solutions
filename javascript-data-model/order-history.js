81b3252d9a67b2522aa826212131e128b3082bd1
Karina Kim <karinaaakim@gmail.com>
2023-10-27 03:34:55 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,22 @@
+const orderHistory = [
+  {
+    books:
+      'JavaScript for impatient programmers' +
+      'The Timeless Way of Building' +
+      'The Art of Sql',
+  },
+  {
+    deliveredOnJuly52020:
+      'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)' +
+      'The Art of Sql',
+    deliveredOnJuly72020: 'Gamecube Controller Adaper',
+    deliveredOnJuly202020: 'The Timeless Way of Building',
+    deliveredOnAug82020: 'Javascript for impatient programmers',
+  },
+  {
+    datesDelivered: 'July5' + 'July7' + 'July20' + 'Aug8',
+  },
+  {
+    gameControllers: 'GameCubeController' + 'Gamecube Controller Adapter',
+  },
+];
